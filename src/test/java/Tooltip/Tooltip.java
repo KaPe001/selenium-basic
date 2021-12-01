f5d8d27cf9a0decc254c11364c7d84de90d80ee4
kpetkowska <kingapetkowska@gmail.com>
2021-12-01 16:23:53 +0100

    tooltip class implementation

---
+++ src/test/java/Tooltip/Tooltip.java
@@ -0,0 +1,33 @@
+package Tooltip;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class Tooltip extends TestBase {
+
+    @Test
+    public void doingToolTips() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("ul li:nth-child(3)")));
+        driver.findElement(By.cssSelector("ul li:nth-child(3)")).click();
+        driver.findElement(By.id("tooltip-item")).click();
+
+        WebElement tooltipMessage = driver.findElement(By.id("age"));
+
+        String getActualMessage = tooltipMessage.getAttribute("title");
+        System.out.println(getActualMessage);
+        assertThat(getActualMessage, equalTo("We ask for your age only for statistical purposes."));
+        logger.info("Tooltip message printed");
+
+
+    }
+}
