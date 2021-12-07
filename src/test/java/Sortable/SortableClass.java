6c8dca1a49f34212982050ab0c09f96d66924afd
kpetkowska <kingapetkowska@gmail.com>
2021-12-07 16:23:56 +0100

    improvements

--- src/test/java/Sortable/SortableClass.java
+++ src/test/java/Sortable/SortableClass.java
@@ -4,18 +4,21 @@ import Alerts.TestBase;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.lang.reflect.Array;
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class SortableClass extends TestBase {
 
     @Test
     public void sortThingsOut() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li:nth-child(2)")));
         driver.findElement(By.cssSelector("li:nth-child(2)")).click();
         driver.findElement(By.id("sortable-item")).click();
@@ -25,22 +28,19 @@ public class SortableClass extends TestBase {
         Collections.shuffle(tableList);
         System.out.println(Arrays.toString(table));
 
-        List<WebElement> WebElementTable = driver.findElements(By.id("sortable"));
-        List<String> beforeShuffling = Arrays.asList(new String[WebElementTable.size()]);
+        List<WebElement> WebElementTable = driver.findElements(By.cssSelector("#sortable li"));
 
-        for(int i = 0; i< WebElementTable.size(); i++){
-            beforeShuffling.set(i, WebElementTable.get(i).getText());
+        for (int i = 1; i <= WebElementTable.size(); i++) {
+            actionOnElement(table[i], i);
         }
-//        System.out.println((beforeShuffling));
-
-        System.out.println("After the shuffling");
-//        Collections.shuffle(beforeShuffling);
-
-        System.out.println(beforeShuffling);
     }
-//    public static void Print(List<String> ar){
-//        for(int i = 0; i < ar.size(); i++){
-//            System.out.println(ar.get(i));
-//        }
-//    }
+
+    public void actionOnElement(int what, int whereTo) {
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(driver.findElement(By.xpath("//*[@id='sortable']/li[" + what + "]")),
+                        driver.findElement(By.cssSelector("#sortable li:nth-child(" + whereTo + ")")))
+                .release()
+                .build()
+                .perform();
+    }
 }
